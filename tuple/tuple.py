b35e67573dff0885546b147d3f52e24f8d3ecfca
LachoLazarov <lacho.lazerov@gmail.com>
2023-11-08 12:40:51 +0200

    tuple

---
+++ tuple/tuple.py
@@ -0,0 +1,5 @@
+# tuple - faster lists you can't change
+friends = ['Eric', 'John', 'Michael', 'Terry', 'Graham']
+tuple = ('Eric','John','Michael','Terry','Graham')
+print(friends)
+print(tuple[2:4])
