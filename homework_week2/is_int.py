ad2d603663699bfa66a39302f4e9e574090d298a
LachoLazarov <lacho.lazerov@gmail.com>
2023-11-16 19:58:48 +0200

    integer and odd numbers functions

--- homework_week2/is_int.py
+++ homework_week2/is_int.py
@@ -1,9 +1,9 @@
 # Create a function that returns True if the argument is an integer and False to any other type of data
+# I am not sure of this - need clarifications!
+def is_integer(argument):
+    return type(argument) == int
 
-user_input = input("Enter number or a text: ")
-
-if user_input.isdigit():
-    print('True')
-else:
-    print('False')
-
+argument_a = is_integer(3)
+argument_b = is_integer("Test text")
+print(argument_a)
+print(argument_b)
--- homework_week2/odd_nrs.py
+++ homework_week2/odd_nrs.py
@@ -1,7 +1,11 @@
 # Write a function that takes a list of numbers and appends only the odd numbers to a new list.
 # Return the new list.
-list = [1,2,3,4,5,6,7,8,9,10]
-new_list = []
-for
-print(new_list)
+def odd_numbers(list):
+    odd_numbers=[]
+    for i in list:
+        if i % 2 != 0:
+            odd_numbers.append(i)
+    return odd_numbers
 
+list = [1,2,3,4,5,6,7,8,9,10]
+print(odd_numbers(list))
